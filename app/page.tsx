c09769cad6a08bd3b8062e48e22eb34157be4a3e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-06 15:06:02 +0530

    Remove boilerplate from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <div className={"text-lg text-red-500"}>TEST</div>
     </main>
   );
