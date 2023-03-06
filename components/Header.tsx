b66862bdf61dced0f08fd74aef4b36e365a9768e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-06 14:30:51 +0530

    Add logo to the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
     <header className="h-75 flex w-full items-center gap-2 px-5 py-5 font-bold text-gray-800 shadow-sm">
-      <div className="header__logo"></div>
-      <p className="mr-auto text-2xl tracking-tight">My Learning Journal</p>
-      <nav className="header__nav">
+      <Image
+        src="/logo.png"
+        alt="Vercel Logo"
+        className={"h-10 w-10 rounded-md object-cover"}
+        width={100}
+        height={24}
+        priority
+      />
+
+      <p className="mr-auto ml-2 text-2xl tracking-tight">
+        My Learning Journal
+      </p>
+      <nav>
         <ul className="nav__container flex gap-5">
           <li className="nav__home nav__item">
             <a href="/article.html">Home</a>
