dfdcad942f1b712571a6a485be943aa5a6776b6b
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-06 14:56:28 +0530

    Add the Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center gap-1 bg-slate-900 py-3 text-cyan-50">
+      <h2>My Learning Journal</h2>
+      <p className="text-sm">Copyright 2023</p>
+    </footer>
+  );
+}
