ab543b74ee6cc088677e428c2a1a71904a1d1f4a
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-06 15:03:13 +0530

    Add schema for post summaries

---
+++ data/postSummaryData.tsx
@@ -0,0 +1,71 @@
+// These are GPT generated
+const postSummaries = [
+  {
+    id: 1,
+    title: "Prototype Management vs. Product Management",
+    summary:
+      "A comparison of two essential management roles that are critical to the success of a product in today's market.",
+    fileName: "prototype-management-vs-product-management",
+    author: "Prashant",
+    date: "2023-02-06",
+    tags: ["product management", "prototype management", "leadership"],
+    image: "article-image-01.png",
+  },
+  {
+    id: 2,
+    title: "5 Tips for Effective Project Management",
+    summary:
+      "Learn the top 5 tips to help you manage your projects more effectively and ensure their successful completion.",
+    fileName: "5-tips-for-effective-project-management",
+    author: "Prashant",
+    date: "2023-02-07",
+    tags: ["project management", "productivity", "time management"],
+    image: "article-image-02.png",
+  },
+  {
+    id: 3,
+    title: "The Importance of User Experience in Design",
+    summary:
+      "User experience is an essential aspect of modern design. This post explores why it's so important and how to improve it.",
+    fileName: "importance-of-user-experience-in-design",
+    author: "Prashant",
+    date: "2023-02-08",
+    tags: ["user experience", "design", "usability"],
+    image: "article-image-03.png",
+  },
+  {
+    id: 4,
+    title: "How to Conduct Effective User Research",
+    summary:
+      "Effective user research is key to creating products that meet the needs of your users. Learn how to do it right.",
+    fileName: "how-to-conduct-effective-user-research",
+    author: "Prashant",
+    date: "2023-02-09",
+    tags: ["user research", "product design", "market research"],
+    image: "article-image-01.png",
+  },
+  {
+    id: 5,
+    title: "10 Principles of Design Thinking",
+    summary:
+      "Design thinking is a powerful problem-solving approach used by successful businesses worldwide. Here are 10 key principles to keep in mind.",
+    fileName: "10-principles-of-design-thinking",
+    author: "Prashant",
+    date: "2023-02-10",
+    tags: ["design thinking", "problem-solving", "innovation"],
+    image: "article-image-02.png",
+  },
+  {
+    id: 6,
+    title: "The Benefits of Agile Development",
+    summary:
+      "Agile development is a popular software development methodology that provides many benefits over traditional development methods. Learn more in this post.",
+    fileName: "benefits-of-agile-development",
+    author: "Prashant",
+    date: "2023-02-11",
+    tags: ["agile development", "software development", "project management"],
+    image: "article-image-03.png",
+  },
+];
+
+export default postSummaries;
